30b508c99c177f7d515dc205601f71039b3ff126
octobertech <octobrtech@gmail.com>
2019-05-09 20:41:02 +0600

    basic functionality

---
+++ scripts.js
@@ -0,0 +1,27 @@
+// @flow
+/* Get our elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+/* Build out functions */
+function togglePlay() {
+	const method = video.paused ? 'play' : 'pause';
+	video[method]();
+	
+}
+
+function updateButton() {
+	toggle.textContent = this.paused ? '►' : '||';
+	
+}
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
